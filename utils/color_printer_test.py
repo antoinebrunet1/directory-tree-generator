868da0ce54c2bb3d17117352337b6856e30dc70b
antoinebrunet1 <antoinebrunet1@outlook.com>
2024-04-22 17:55:52 -0400

    Unit test added for color_printer and code added to run color_printer_test without using terminal

--- utils/color_printer_test.py
+++ utils/color_printer_test.py
@@ -1,9 +1,16 @@
 import unittest
+from unittest import mock
+
 import color_printer
 from colorama import Fore
 
 
 class TestColorPrinter(unittest.TestCase):
+    @mock.patch("colorama.init")
+    def test_init_with_auto_reset_calls_init(self, mocked_init):
+        color_printer.init_with_auto_reset()
+        mocked_init.assert_called_with(autoreset=True)
+
     def test_get_colored_file_name(self):
         file_name = "test.txt"
         actual_result = color_printer.get_colored_file_name(file_name)
@@ -17,3 +24,7 @@ class TestColorPrinter(unittest.TestCase):
         expected_result = f"{Fore.MAGENTA}{directory_name}{Fore.RESET}"
 
         assert actual_result == expected_result
+
+
+if __name__ == "__main__":
+    unittest.main()
