5f2c9bc3fa0eff885142f3e9bed525a3453cba55
antoinebrunet1 <antoinebrunet1@outlook.com>
2024-04-22 17:23:51 -0400

    Changed assertions for color_printer tests to match with the ones of directory_tree_generator

--- utils/color_printer_test.py
+++ utils/color_printer_test.py
@@ -9,11 +9,11 @@ class TestColorPrinter(unittest.TestCase):
         actual_result = color_printer.get_colored_file_name(file_name)
         expected_result = f"{Fore.BLUE}{file_name}"
 
-        self.assertEqual(actual_result, expected_result)
+        assert actual_result == expected_result
 
     def test_get_colored_directory_name(self):
         directory_name = "test"
         actual_result = color_printer.get_colored_directory_name(directory_name)
         expected_result = f"{Fore.MAGENTA}{directory_name}{Fore.RESET}"
 
-        self.assertEqual(actual_result, expected_result)
+        assert actual_result == expected_result
